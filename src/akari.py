c92e99cbd20863aaa126f5fe97be1d37e2b56644
Borroot <borroot@live.nl>
2020-09-20 23:07:30 +0200

    Add uniqueness testing function.

--- src/akari.py
+++ src/akari.py
@@ -2,12 +2,14 @@ from z3 import *
 
 from printer import display
 from loader import *
-from z3solver import z3solve
+from z3solver import z3solve, z3unique
 
 
 def main():
-    puzzle = loadpuzzle('misc/25x25_hard', 0, 25, 25)
-    solved, solution, time = z3solve(puzzle)
+    puzzle = loadpuzzle('misc/14x14_hard', 0, 14, 14)
+    solution = z3solve(puzzle)
+    display(puzzle, solution)
+    print('The puzzle is unique.' if z3unique(puzzle) else 'The puzzle is not unique.')
 
 
 if __name__ == '__main__':
--- src/z3solver.py
+++ src/z3solver.py
@@ -4,25 +4,53 @@ import time
 from constraints import constraints_all
 
 
-def _solution(positions, model, bvars):
+def _solution(positions, bvars, model):
     return [(x, y) for (x, y) in positions if model[bvars[x, y]]]
 
 
-def z3solve(puzzle):
+def _initialize(puzzle):
     positions = [(x, y) for y in range(len(puzzle)) for x in range(len(puzzle[0]))]
     bvars = {(x, y): Bool("v{};{}".format(x, y)) for x, y in positions}
 
     solver = Solver()
     constraints_all(puzzle, solver, positions, bvars)
 
-    start = time.time()
-    solved = solver.check() == sat
-    end = time.time()
+    return positions, bvars, solver
 
-    if solved:
-        model = solver.model()
-        solution = _solution(positions, model, bvars)
+
+def z3solve(puzzle, timed=False):
+    positions, bvars, solver = _initialize(puzzle)
+
+    if timed: start = time.time()
+    result = solver.check()
+    if timed: end = time.time()
+
+    if result == sat:
+        solution = _solution(positions, bvars, solver.model())
     else:
         solution = None
 
-    return solved, solution, end - start
+    if timed:
+        return solution, end - start
+    else:
+        return solution
+
+
+def z3unique(puzzle, timed=False):
+    positions, bvars, solver = _initialize(puzzle)
+
+    if timed: start = time.time()
+
+    unique = None
+    if solver.check() == sat:
+        solution = _solution(positions, bvars, solver.model())
+        constraints = Not(And([bvars[x, y] for (x, y) in solution]))
+        solver.add(constraints)
+        unique = solver.check() != sat
+
+    if timed: end = time.time()
+
+    if timed:
+        return unique, end - start
+    else:
+        return unique
