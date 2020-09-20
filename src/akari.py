41065d8475324a90f30e57e82adb04444f4cfc88
Borroot <borroot@live.nl>
2020-09-21 00:26:58 +0200

    Add hans his puzzles and a hans puzzle loader.

--- src/akari.py
+++ src/akari.py
@@ -6,10 +6,15 @@ from z3solver import z3solve, z3solves, z3unique
 
 
 def main():
-    for index in range(999):
-        puzzle = loadpuzzle('misc/14x14_hard', index, 14, 14)
-        solutions, time = z3solves(puzzle, True)
-        print(index, len(solutions), time)
+    # for index in range(999):
+        # puzzle = loadpuzzle('misc/web/14x14_hard', index, 14, 14)
+        # solution, time = z3solves(puzzle, True)
+        # print(index, time)
+
+    for i in range(1, 10):
+        puzzle = loadhans('misc/hans/r' + str(i) + 's')
+        solution, time = z3solve(puzzle, True)
+        print(f'r{i}s', time)
 
 
 if __name__ == '__main__':
--- src/loader.py
+++ src/loader.py
@@ -6,6 +6,27 @@ def loadpuzzle(filename, index, height, width):
     return _convert(codex, height, width)
 
 
+def loadhans(filename):
+    with open(filename) as fp:
+        width = height = int(fp.readline())
+        puzzle = [list(range(width)) for _ in range(height)]
+
+        count = 0
+        for _ in range(height):
+            for c in fp.readline():
+                if c == '*':
+                    puzzle[count // height][count % width] = B
+                    count += 1
+                elif c in '01234':
+                    puzzle[count // height][count % width] = ord(c) - ord('0')
+                    count += 1
+                elif c == '-':
+                    puzzle[count // height][count % width] = N
+                    count += 1
+
+        return puzzle
+
+
 def _loadcodex(filename, index):
     with open(filename) as fp:
         for i, line in enumerate(fp):
